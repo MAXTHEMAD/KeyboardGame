136c911abc36836e56aeb173ae30e993ad9d71d6
AHannis <117646416+AHannis@users.noreply.github.com>
2023-11-29 14:15:59 +0000

    Spline 1

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public float smooth = 5.0f;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, target.position,
+            Time.deltaTime * smooth);
+    }
+
+}
